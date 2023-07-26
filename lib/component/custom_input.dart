17d19a79c43c1a530046acd84f71a882c268868a
seokhyeon-Park <dfbgq346q@gmail.com>
2023-07-26 13:52:25 +0900

    *

---
+++ lib/component/custom_input.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget {
+  final Color mainColor;
+  final Color subColor;
+  final double widthPer;
+  final String hint;
+
+  const CustomInput({
+    super.key,
+    required this.mainColor,
+    required this.subColor,
+    required this.widthPer,
+    required this.hint,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * widthPer,
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hint,
+          filled: true,
+          // fillColor: mainColor,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: mainColor,
+              width: 1.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: subColor,
+              width: 1.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_demo/component/custom_input.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,59 +6,135 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Flexible(
-          flex: 3,
-          child: Container(
-            alignment: Alignment.center,
-            decoration:
-                const BoxDecoration(color: Color.fromRGBO(58, 166, 185, 1.0)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: Container(
-                    width: 120,
-                    height: 120,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      // @유저 프로필 사진
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/pig.jpeg'),
-                        fit: BoxFit.fill,
+    Color mainColor = const Color.fromRGBO(58, 166, 185, 1.0);
+    Color subColor = const Color.fromRGBO(255, 158, 170, 1.0);
+
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          children: [
+            Flexible(
+              flex: 3,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: mainColor,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 80),
+                      child: Container(
+                        width: 120,
+                        height: 120,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          // @유저 프로필 사진
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/pig.jpeg'),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        'Honeypig Kaizoku',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 30),
-                  child: Text(
-                    'Honey Pig Kaizoku',
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
+              ),
+            ),
+            Flexible(
+              flex: 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 22.0),
+                    child: CustomInput(
+                      mainColor: mainColor,
+                      subColor: subColor,
+                      widthPer: 0.85,
+                      hint: 'ID',
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 22.0),
+                    child: CustomInput(
+                      mainColor: mainColor,
+                      subColor: subColor,
+                      widthPer: 0.85,
+                      hint: 'PW',
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            shadowColor: Colors.transparent,
+                            backgroundColor: mainColor,
+                            fixedSize: const Size(150, 44),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'sign up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(14),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            shadowColor: Colors.transparent,
+                            backgroundColor: subColor,
+                            fixedSize: const Size(150, 44),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'log in',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
+          ],
         ),
-        Flexible(
-          flex: 3,
-          child: Container(
-            decoration: const BoxDecoration(color: Colors.white),
-          ),
-        ),
-        Flexible(
-          flex: 3,
-          child: Container(
-            decoration: const BoxDecoration(color: Colors.white),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }

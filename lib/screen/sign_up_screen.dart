d02e6d7284e9d50b61354e3de494e09f52a778fb
seokhyeon-Park <dfbgq346q@gmail.com>
2023-08-03 14:31:10 +0900

    *

--- lib/screen/sign_up_screen.dart
+++ lib/screen/sign_up_screen.dart
@@ -1,9 +1,11 @@
 import 'package:chat_demo/component/rounded_event_button.dart';
 import 'package:chat_demo/component/rounded_input.dart';
 import 'package:chat_demo/pack/login_header.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   final Color mainColor;
   final Color subColor;
 
@@ -14,13 +16,66 @@ class SignUpScreen extends StatelessWidget {
   });
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  var userId = '';
+  var userPw = '';
+  var userName = '';
+  int userWeight = 100;
+  Color mintColor = const Color.fromRGBO(193, 236, 228, 1.0);
+
+  final ImagePicker _picker = ImagePicker();
+
+  getImg() async {
+    // 이미지 불러오기
+    XFile? selectImage = await _picker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 75,
+      maxWidth: 75,
+      imageQuality: 30,
+    );
+
+    if (selectImage != null) {
+      // 이미지 물리 경로
+      dynamic sendData = selectImage.path;
+      var formData = FormData.fromMap(
+        {'image': await MultipartFile.fromFile(sendData)},
+      );
+
+      print(formData);
+    }
+  }
+
+  getId(id) {
+    userId = id;
+  }
+
+  getPw(pw) {
+    userPw = pw;
+  }
+
+  getName(name) {
+    userName = name;
+  }
+
+  getWeight(weight) {
+    if (weight == '') {
+      userWeight = 100;
+    } else {
+      userWeight = int.parse(weight);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        backgroundColor: mainColor,
+        backgroundColor: widget.mainColor,
         resizeToAvoidBottomInset: false,
         body: Column(
           children: [
@@ -43,10 +98,10 @@ class SignUpScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 22.0),
                     child: RoundedInput(
                       returnText: (text) {
-                        print(text);
+                        getId(text);
                       },
-                      mainColor: mainColor,
-                      subColor: subColor,
+                      mainColor: widget.mainColor,
+                      subColor: widget.subColor,
                       widthPer: 0.85,
                       inputType: TextInputType.text,
                       encrypt: false,
@@ -57,10 +112,10 @@ class SignUpScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 22.0),
                     child: RoundedInput(
                       returnText: (text) {
-                        print(text);
+                        getPw(text);
                       },
-                      mainColor: mainColor,
-                      subColor: subColor,
+                      mainColor: widget.mainColor,
+                      subColor: widget.subColor,
                       widthPer: 0.85,
                       inputType: TextInputType.text,
                       encrypt: true,
@@ -71,10 +126,10 @@ class SignUpScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 22.0),
                     child: RoundedInput(
                       returnText: (text) {
-                        print(text);
+                        getName(text);
                       },
-                      mainColor: mainColor,
-                      subColor: subColor,
+                      mainColor: widget.mainColor,
+                      subColor: widget.subColor,
                       widthPer: 0.85,
                       inputType: TextInputType.text,
                       encrypt: false,
@@ -85,14 +140,27 @@ class SignUpScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 22.0),
                     child: RoundedInput(
                       returnText: (text) {
-                        print(text);
+                        getWeight(text);
                       },
-                      mainColor: mainColor,
-                      subColor: subColor,
+                      mainColor: widget.mainColor,
+                      subColor: widget.subColor,
                       widthPer: 0.85,
                       inputType: TextInputType.number,
                       encrypt: false,
                       hint: 'WEIGHT (Default : 100)',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 22.0),
+                    child: RoundedEventButton(
+                      title: 'Profile image',
+                      rounded: 30,
+                      color: mintColor,
+                      fontColor: Colors.black,
+                      fontSize: 16,
+                      buttonWidth: MediaQuery.of(context).size.width * 0.4,
+                      buttonHeight: 40,
+                      onTapEvent: getImg,
                     ),
                   ),
                   Row(
@@ -103,12 +171,15 @@ class SignUpScreen extends StatelessWidget {
                         child: RoundedEventButton(
                           buttonHeight: 44,
                           buttonWidth: 80,
-                          color: subColor,
+                          color: widget.subColor,
                           rounded: 30,
                           fontSize: 19,
                           fontColor: Colors.white,
                           title: '● ●',
-                          onTapEvent: () {},
+                          onTapEvent: () {
+                            // if 정상적으로 작성 및 가입이 된 경우.
+                            Navigator.pop(context);
+                          },
                         ),
                       ),
                     ],
